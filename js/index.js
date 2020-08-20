6a5fecbc61186c748ca405d6ebbea8578937863d
Brittani Ericksen <heybrittani@gmail.com>
2020-08-20 16:15:39 -0400

    Added more functionality. All operator button are functional and math is correct

--- js/index.js
+++ js/index.js
@@ -1,33 +1,103 @@
 'use strict';
 
-const inputScreen = document.getElementById('input');
-const inputArray = [];
+const inputScreen = document.querySelector('#input'),
+    numberButtons = document.querySelectorAll('.number'),
+    operButtons = document.querySelectorAll('.operator'),
+    result = document.querySelector('#result'),
+    clear = document.querySelector('#clear');
 
-const numberButtons = document.querySelectorAll('.number');
-numberButtons.forEach(function(element) {
-    element.addEventListener('click', function(event) {
-        event.preventDefault();
-        inputArray.push(element.innerHTML);
-        inputScreen.innerText += element.innerHTML;
-    })
-})
+let resultDisplayed = false; 
 
-const operButtons = document.querySelectorAll('.operator');
-const inputArrayOperator = [];
-operButtons.forEach(function(element) {
-    element.addEventListener('click', function(event) {
-        event.preventDefault();
+// create empty array for input
+let numberOperatorsArray = [];
 
-        let currentValues = inputScreen.innerText;
-        let lastCharacter = currentValues[currentValues.length - 1];
+// number buttons event listener
+numberButtons.forEach(function(number) {
+    number.addEventListener('click', function() {
+        inputScreen.innerText += this.innerHTML;
+        numberOperatorsArray = [...numberOperatorsArray, this.innerHTML];
+    });
+});
 
-        if (lastCharacter === '+' || lastCharacter === '-' || lastCharacter === '*' || lastCharacter === '/') {
-            const newString = currentValues.substring(0, currentValues.length - 1) + element.innerHTML;
-            inputScreen.innerText = newString;
-        } else if (currentValues.length === 0) {
-            alert("TYPE A NUMBER");
+// operator buttons event listener
+operButtons.forEach(function(operator) {
+    operator.addEventListener('click', function() {
+        inputScreen.innerText += this.innerHTML;
+        numberOperatorsArray = [...numberOperatorsArray, this.innerHTML];
+    });
+}); 
+
+// on click of 'equal' button
+result.addEventListener('click', function() {
+    let numbersStringHolder = "";
+    let resultArray = [];
+    for (let char of numberOperatorsArray) {
+        const numReg = /\d/;
+        if (numReg.test(char) || char === '.') {
+            numbersStringHolder += char;
         } else {
-            inputScreen.innerText += element.innerHTML;
+            resultArray = [...resultArray, Number(numbersStringHolder), char];
+            numbersStringHolder = "";
         }
-    })
-})
+    }
+    resultArray = [...resultArray, Number(numbersStringHolder)];
+    
+    let multiply = resultArray.indexOf("*");
+    while (multiply !== -1) {
+        resultArray.splice(multiply-1, 3, resultArray[multiply-1] * resultArray[multiply+1]);
+        multiply = resultArray.indexOf("*");
+    }
+
+    let divide = resultArray.indexOf("/");
+    while (divide !== -1) {
+        resultArray.splice(divide-1, 3, resultArray[divide-1] / resultArray[divide+1]);
+        divide = resultArray.indexOf("/");
+    }
+
+    let add = resultArray.indexOf("+");
+    while (add !== -1) {
+        resultArray.splice(add-1, 3, resultArray[add-1] + resultArray[add+1]);
+        add = resultArray.indexOf("+");
+    }
+
+    let subtract = resultArray.indexOf("-");
+    while (subtract !== -1) {
+        resultArray.splice(subtract-1, 3, resultArray[subtract-1] - resultArray[subtract+1]);
+        subtract = resultArray.indexOf("-");
+    }
+
+
+    console.log("result array: ", resultArray);
+    numberOperatorsArray = [...resultArray];
+    inputScreen.innerHTML = numberOperatorsArray;
+
+
+});
+
+// clearing the input on press of clear
+clear.addEventListener('click', function() {
+    inputScreen.innerHTML = "";
+    numberOperatorsArray = [];
+});
+
+
+
+
+
+
+
+
+
+//         let currentValues = inputScreen.innerText;
+//         let lastCharacter = currentValues[currentValues.length - 1];
+
+//         if (lastCharacter === '+' || lastCharacter === '-' || lastCharacter === '*' || lastCharacter === '/') {
+//             const newString = currentValues.substring(0, currentValues.length - 1) + element.innerHTML;
+//          inputScreen.innerText = newString;
+//         } else if (currentValues.length === 0) {
+//             alert("TYPE A NUMBER");
+//         } else {
+//          inputScreen.innerText += element.innerHTML;
+//         }
+//     });
+// });
